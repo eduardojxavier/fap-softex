5f3a32c394f88252f7b3fdc7adf4c5378e64ad85
eduardojxavier <eduardojxavier@gmail.com>
2023-08-12 23:57:36 -0300

    Signed-off-by: eduardojxavier <eduardojxavier@gmail.com>

---
+++ listaEncadeada.js
@@ -0,0 +1,5 @@
+const listaEncadeada = [
+    { nome: 'Gabriela', idade: 30, filho: lista[1] },
+    { nome: 'Eduardo', idade: 31, filho: lista[2] },
+    { nome: 'Maria Helena', idade: 5, filho: null }
+  ]
